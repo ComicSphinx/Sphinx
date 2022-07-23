5c93bfb4bb781ccb6916488e9784cc964884fcad
Daniil Maslov <maslov2daniil@gmail.com>
2022-07-23 16:11:00 +0400

    Добавил authorization service

--- sphinx/urls.py
+++ sphinx/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('budget/', include('budget.urls')),
-    path('invest/', include('invest.urls'))
+    path('invest/', include('invest.urls')),
+    path('authorization/', include('authorization.urls'))
 ]
