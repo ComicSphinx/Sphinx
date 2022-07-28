d0498cf7e493e2254455a4d6fc9ca7f38e1b759e
Daniil Maslov <maslov2daniil@gmail.com>
2022-07-29 00:31:27 +0400

    Стер все, чтобы начать заново по офф.гайду https://docs.djangoproject.com/en/4.0/topics/auth/

--- account/views.py
+++ account/views.py
@@ -4,21 +4,5 @@ from django.template import loader
 from django.contrib.auth import authenticate, login
 from account.forms import LoginForm
 
-
 def user_login(request):
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            credentials = form.cleaned_data
-            user = authenticate(username=credentials['login'], password=credentials['password'])
-            if user is not None:
-                if user.is_active:
-                    login(request, user)
-                    return HttpResponse('Успешно авторизован')
-                else:
-                    return HttpResponse("Не активный аккаунт")
-            else:
-                return HttpResponse("Введены некорретные  данные")
-    else:
-        form = LoginForm()
-    return render(request, 'account/login.html', {'form': form})
+    return ''
