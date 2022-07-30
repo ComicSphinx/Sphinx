f1b47ed24f178e274425ea529654f53dc375bbd6
Daniil Maslov <maslov2daniil@gmail.com>
2022-07-31 00:35:43 +0400

    Логаут работает идеально, вручную настроил пути

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,18 +2,9 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('', include('django.contrib.auth.urls')),
-    # This include the following URL patterns:
-    # accounts/login/ [name='login']
-    # accounts/logout/ [name='logout']
-    # accounts/password_change/ [name='password_change']
-    # accounts/password_change/done/ [name='password_change_done']
-    # accounts/password_reset/ [name='password_reset']
-    # accounts/password_reset/done/ [name='password_reset_done']
-    # accounts/reset/<uidb64>/<token>/ [name='password_reset_confirm']
-    # accounts/reset/done/ [name='password_reset_complete']
-    
-    path('registration/', views.registration, name='registration'),
-    path('profile/', views.profile, name='profile'),
-    path('change_password/', views.change_password, name='change password')
+path('login/', views.login_view, name='login_view'),
+    path('logout/', views.logout_view, name='logout_view'),
+    path('registration/', views.registration_view, name='registration_view'),
+    path('profile/', views.profile_view, name='profile_view'),
+    path('change_password/', views.change_password_view, name='change_password_view')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.decorators import login_required
 
 from .forms import LoginForm, RegistrationForm, ChangePasswordForm
 
-def login(request):
+def login_view(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
@@ -16,16 +16,16 @@ def login(request):
             user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
-                return HttpResponseRedirect('/budget')
+                return HttpResponseRedirect('/accounts/profile')
     else: 
         form = LoginForm()
+        return render(request, 'login.html', {'form': form})
 
-@login_required(login_url='/accounts/login/')
-def logout(request):
+def logout_view(request):
     logout(request)
-    return HttpResponse('logout')
+    return HttpResponseRedirect('/accounts/profile')
 
-def registration(request):
+def registration_view(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
@@ -38,11 +38,11 @@ def registration(request):
         return render(request, 'registration.html', {'form': form})
 
 @login_required(login_url='/accounts/login/')
-def profile(request):
+def profile_view(request):
     return render(request, 'profile.html')
 
 @login_required(login_url='/accounts/login/')
-def change_password(request):
+def change_password_view(request): # TODO оно не работает (не меняет пароль, хотя возвращает успешный response). Возможно, я не сохраняю?
     if request.method == 'POST':
         username = request.user.username
         new_password = request.POST['password']
