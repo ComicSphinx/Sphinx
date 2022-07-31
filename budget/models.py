c8f8fc01f805b9f817b22a670028de2d511d8114
Daniil Maslov <maslov2daniil@gmail.com>
2022-07-31 19:55:44 +0400

    Удалил из модели BudgetFields, скорректировал Budget и добавли ему метод serialize

--- budget/models.py
+++ budget/models.py
@@ -1,13 +1,19 @@
+from statistics import mode
 from django.db import models
+from django.contrib.auth.models import User
 
-class BudgetFields(models.Model):
-    # + userId (связать, когда появится авторизация и userid, я буду их где-то хранить) TODO: создать класс "пользователь"
-    name = models.CharField(max_length=100)
-    active = models.BooleanField()
-
-
+# TODO refactor it, at least name
 class Budget(models.Model):
-    # + userId (связать, когда появится авторизация и userid, я буду их где-то хранить) TODO: создать класс "пользователь"
-    budgetFieldId = models.ForeignKey(BudgetFields, on_delete=models.PROTECT)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    field_name = models.CharField(max_length=100)
     value = models.CharField(max_length=100)
     active = models.BooleanField()
+
+    def serialize(self):
+        # TODO сделать так, чтобы возвращались только активные (если я не сделаю это в шаблоне)
+        return {
+            'user': self.user,
+            'field_name': self.field_name,
+            'value': self.value,
+            'active': self.active
+        }
