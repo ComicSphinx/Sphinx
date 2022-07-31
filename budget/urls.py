bf44b01010791c948417e62444da2e5f68a31566
Daniil Maslov <maslov2daniil@gmail.com>
2022-07-31 19:57:01 +0400

    Добавил метод добавления поля и значения в бд (и сделал render этих данных в budget_view)

--- budget/urls.py
+++ budget/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.budget_view, name='budget_view')
+    path('', views.budget_view, name='budget_view'),
+    path('add_field/', views.add_field_to_db, name='add_field_to_db')
 ]
--- budget/views.py
+++ budget/views.py
@@ -1,9 +1,20 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from sqlalchemy import true
 
 from .forms import AddFieldForm
+from .models import Budget
 
 @login_required(login_url='/accounts/login/')
 def budget_view(request):
     form = AddFieldForm()
-    return render(request, 'budget.html', {'form': form})
+    budget_fields = Budget.objects.filter(user=request.user)
+    return render(request, 'budget.html', {'form': form}, budget_fields)
+
+# TODO refactor it (name, at least)
+def add_field_to_db(request):
+    field_name = request.POST['field_name']
+    field_value = request.POST['field_value']
+    record = Budget(user=request.user, field_name=field_name, field_value=field_value, active=True)
+    record.save()
+    return redirect('/budget/')
