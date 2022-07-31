3adc43083dd71b18d3328427d0391934a4278a0d
Daniil Maslov <maslov2daniil@gmail.com>
2022-07-31 18:18:08 +0400

    Переместил шаблоны в templates/ из templates/registration

--- sphinx/settings.py
+++ sphinx/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'sphinx.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'accounts/templates'), os.path.join(BASE_DIR, 'accounts/templates/registration'),],
+        'DIRS': [os.path.join(BASE_DIR, 'accounts/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
