7ab1df3efc27e1f19fb678de3a618c96283a02a1
Daniil Maslov <maslov2daniil@gmail.com>
2022-07-31 18:18:16 +0400

    Добавил табуляцию

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,8 @@
-from django.urls import include, path
+from django.urls import path
 from . import views
 
 urlpatterns = [
-path('login/', views.login_view, name='login_view'),
+    path('login/', views.login_view, name='login_view'),
     path('logout/', views.logout_view, name='logout_view'),
     path('registration/', views.registration_view, name='registration_view'),
     path('profile/', views.profile_view, name='profile_view'),
