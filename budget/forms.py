9988511104e79def9a3bd522787e02919588c4c8
Daniil Maslov <maslov2daniil@gmail.com>
2022-07-31 19:56:07 +0400

    Добавил еще одно поле в форму

--- budget/forms.py
+++ budget/forms.py
@@ -2,3 +2,4 @@ from django import forms
 
 class AddFieldForm(forms.Form):
     field_name = forms.CharField(label='field name:', max_length=30)
+    field_value = forms.CharField(label='field value', max_length=30)
