0cf024b4a6dbbe81b32b945c0a0fc8a5dc93216b
Daniil Maslov <invicems@gmail.com>
2022-08-06 03:22:31 +0400

    Удалил сериализатор

--- budget/models.py
+++ budget/models.py
@@ -5,15 +5,6 @@ from django.contrib.auth.models import User
 # TODO refactor it, at least name
 class Budget(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    field_name = models.CharField(max_length=100, default='null')
-    field_value = models.CharField(max_length=100, default='null')
+    field_name = models.TextField(default='null')
+    field_value = models.TextField(default='null')
     active = models.BooleanField()
-
-    def serialize(self):
-        # TODO сделать так, чтобы возвращались только активные (если я не сделаю это в шаблоне)
-        return {
-            'user': self.user,
-            'field_name': self.field_name,
-            'field_value': self.field_value,
-            'active': self.active
-        }
