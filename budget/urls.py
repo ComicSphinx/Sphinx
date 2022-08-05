2dec0cddfb9621c37f361ac8d96c051ec99a4bf9
Daniil Maslov <invicems@gmail.com>
2022-08-06 03:23:23 +0400

    Добавил метод обновления значений (названия колонки и значения) и связал с фронтом. Оно работает, за исключением того, что символы после пробела обрезаются

--- budget/urls.py
+++ budget/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.budget_view, name='budget_view'),
-    path('add_field/', views.add_field_to_db, name='add_field_to_db')
+    path('add_field/', views.add_field_to_db, name='add_field_to_db'),
+    path('update_field/', views.update_field, name='update_field')
 ]
--- budget/views.py
+++ budget/views.py
@@ -7,9 +7,9 @@ from .models import Budget
 
 @login_required(login_url='/accounts/login/')
 def budget_view(request):
-    form = AddFieldForm()
+    add_field_form = AddFieldForm()
     budget_fields = Budget.objects.filter(user_id=request.user)
-    return render(request, 'budget.html', {'form': form, 'queryset': budget_fields})
+    return render(request, 'budget.html', {'add_field_form': AddFieldForm, 'queryset': budget_fields})
 
 # TODO refactor it (name, at least)
 def add_field_to_db(request):
@@ -17,4 +17,19 @@ def add_field_to_db(request):
     field_value = request.POST['field_value']
     record = Budget(user_id=request.user, field_name=field_name, field_value=field_value, active=True)
     record.save()
+    return redirect('/budget/')
+
+def update_field(request):
+    field_id = request.POST['field_id']
+    field_name = request.POST['field_name']
+    field_value = request.POST['field_value']
+    
+    # TODO: Скорректировать, чтобы обязательные поля (id не нужно было явно указывать)
+    field = Budget(id=field_id)
+    field.user_id = request.user
+    field.field_id = field_id
+    field.field_name = field_name
+    field.field_value = field_value
+    field.active = True
+    field.save()
     return redirect('/budget/')
