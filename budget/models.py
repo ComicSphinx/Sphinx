6f7f6daf497318e408588868c8e81a152cc543aa
Daniil Maslov <invicems@gmail.com>
2022-08-11 01:27:59 +0300

    Добавил таблицы BudgetByMonths и BudgetByYears

--- budget/models.py
+++ budget/models.py
@@ -9,3 +9,18 @@ class Budget(models.Model):
     field_value = models.TextField(default='null') # TODO сделать это поле числовым, вместе с инпутом(чтобы принимал только числа)
     created_date = models.DateField(auto_now_add=True)
     active = models.BooleanField()
+
+class BudgetByMonths(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    field_id = models.ForeignKey(Budget, on_delete=models.CASCADE)
+    field_name = models.TextField(default='null')
+    field_value = models.TextField(default='null') # TODO сделать это поле числовым, вместе с инпутом(чтобы принимал только числа)
+    year_number = models.TextField(default='null') # TODO сделать это поле числовым, (от 1 до 12)
+    active = models.BooleanField() # TODO это поле надо деактивировать, если деактивировано поле active в Budget
+
+class BudgetByYears(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    field_id = models.ForeignKey(Budget, on_delete=models.CASCADE)
+    field_value = models.TextField(default='null') # TODO сделать это поле числовым, вместе с инпутом(чтобы принимал только числа)
+    month_number = models.TextField(default='null') # TODO сделать это поле числовым, (от 1 до 12)
+    active = models.BooleanField() # TODO это поле надо деактивировать, если деактивировано поле active в Budget
