2c2dc7c6e3b71cdc5f47f69ef7dca3df84dab811
Daniil Maslov <invicems@gmail.com>
2022-08-12 00:52:13 +0300

    Сделал поле field_value числовым

--- budget/models.py
+++ budget/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class Budget(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     field_name = models.TextField(default='null')
-    field_value = models.TextField(default='null') # TODO сделать это поле числовым, вместе с инпутом(чтобы принимал только числа)
+    field_value = models.BigIntegerField(default='null') # TODO сделать это поле числовым, вместе с инпутом(чтобы принимал только числа)
     created_date = models.DateField(auto_now_add=True)
     active = models.BooleanField()
 
@@ -14,7 +14,7 @@ class BudgetByMonths(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     field_id = models.ForeignKey(Budget, on_delete=models.CASCADE)
     field_name = models.TextField(default='null')
-    field_value = models.TextField(default='null') # TODO сделать это поле числовым, вместе с инпутом(чтобы принимал только числа)
+    field_value = models.BigIntegerField(default='null') # TODO сделать это поле числовым, вместе с инпутом(чтобы принимал только числа)
     month_number = models.TextField(default='null') # TODO сделать это поле числовым, (от 1 до 12)
     active = models.BooleanField() # TODO это поле надо деактивировать, если деактивировано поле active в Budget
 
@@ -22,6 +22,6 @@ class BudgetByYears(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     field_id = models.ForeignKey(Budget, on_delete=models.CASCADE)
     field_name = models.TextField(default='null')
-    field_value = models.TextField(default='null') # TODO сделать это поле числовым, вместе с инпутом(чтобы принимал только числа)
+    field_value = models.BigIntegerField(default='null') # TODO сделать это поле числовым, вместе с инпутом(чтобы принимал только числа)
     year_number = models.TextField(default='null') # TODO сделать это поле числовым
     active = models.BooleanField() # TODO это поле надо деактивировать, если деактивировано поле active в Budget
